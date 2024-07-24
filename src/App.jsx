58a3c2aad64e16e9acafd0aa819922d5f9932958
Alecaring <Private_a.a.c@icloud.com>
2024-07-24 03:50:44 +0200

    add: FruitComponent and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
+const fruits = [
+  {
+    id: 1,
+    typeOfFruit: 'frutta',
+    fruit: 'banana'
+  },
+  {
+    id: 2,
+    typeOfFruit: 'frutta',
+    fruit: 'arancia'
+  },
+  {
+    id: 3,
+    typeOfFruit: 'verdura',
+    fruit: 'spinaci'
+  },
+  {
+    id: 4,
+    typeOfFruit: 'legumi',
+    fruit: 'lenticchie'
+  },
+];
+
+
 function App() {
   return (
-    <div>
-      ciao
+    <div className="container">
+      <ul className="list-group-style">
+        {fruits.map((fruit) => (
+          <FruitComponet
+            key={fruit.id}
+            fruit={fruit.fruit}
+            typeOfFruit={fruit.typeOfFruit} />
+        ))}
+      </ul>
     </div>
   );
 };
+
+function FruitComponet({ fruit, key, typeOfFruit }) {
+  
+  let className = '';
+  typeOfFruit === 'verdura'? className = 'verdura' : '';
+  typeOfFruit === 'legumi'? className = 'legumi' : '';
+  typeOfFruit === 'frutta'? className = 'frutta' : '';
+ 
+  return (
+    <li key={ key }>
+      <div className={ className }>
+    <span>{ fruit }</span>
+      </div>
+    </li>
+  )
+}
 
 export default App;
